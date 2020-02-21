5f464bb44da589d086599879a3d2c57d7c3fbaa7
belladewusa <55181780+belladewusa@users.noreply.github.com>
2020-02-21 16:20:34 +0700

    Add files via upload

---
+++ shift3a.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+c=`ls | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
+
+if [[ $c =~ [^0-9] ]]
+then
+	c=0
+fi
+
+a=`expr $c + 1`
+b=`expr $c + 29`
+
+for ((i=a;i<+b;i++))
+do
+	wget -a $PWD/wget.log -O $PWD/"pdkt_kusuma_$i" https://loremflickr.com/320/240/cat
+done
